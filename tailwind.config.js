c601698d668662a84e2c60f71afa3fd27b1aae80
Vlada45 <94285852+Vlada45@users.noreply.github.com>
2026-02-03 21:40:13 +0100

    flashcard design

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+            },
+        },
+    },
+}
